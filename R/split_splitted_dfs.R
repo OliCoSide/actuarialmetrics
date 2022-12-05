4e9e57d04015aa7402c010a5e5f00b9342cb0b2f
Olivier Côté <45428376+OliCoSide@users.noreply.github.com>
2022-12-05 11:44:43 -0500

    moving out

--- R/split_splitted_dfs.R
+++
@@ -1,7 +0,0 @@
-#' @export
-split_the_splitted_df <- function(split_df, split_id){
-  list("X" = split_df$X[split_id],
-       "D" = split_df$D[split_id],
-       "response" = split_df$response,
-       "exposure" = split_df$exposure)
-}
