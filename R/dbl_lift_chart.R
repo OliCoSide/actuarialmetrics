36a3cec501bb3bfa5eb373045577d296f524fc60
Olivier Côté <45428376+OliCoSide@users.noreply.github.com>
2022-12-15 12:17:40 -0500

    documentation + licence

--- R/dbl_lift_chart.R
+++ R/dbl_lift_chart.R
@@ -1,39 +1,54 @@
-
+#' Display a \emph{double lift chart} with \code{ggplot2} synthax
+#'
+#' @param table_list A table produced by \code{get_lr_table}
+#' @param Prem_names The names of the ratemaking structure to compare. The default is given in \code{table_list}
+#' @param palette The palette to use for the visualisation (from \code{Rcolorbrewer}). The default is \code{"Dark2"}.
+#' @return A \code{ggplot2} graph of a \emph{double lift chart}
+#' @examples
+#' library(actuarialmetrics)
+#' data("pred_table")
+#' table_to_g <- get_lr_table(pred_table,
+#'                            ref_name = "mod2",
+#'                              comp_name = "mod3",
+#'                              loss_name = "Y",
+#'                              n_cuts = 6)
+#'
+#' dbl_lift_chart(table_to_g)
 #' @export
 dbl_lift_chart <- function(table_list,
                            Prem_names = c(table_list$ref_name,
                                           table_list$comp_name),
                            palette = "Dark2"){
-  
-  ## we get the table from the list 
+
+  ## we get the table from the list
   table <- table_list$table
-  
+
   n_cuts <- nrow(table)
-  # # couleurs 
+  # # couleurs
   # col <- hcl.colors(n_cuts, colors)
   # col_dark <- rgb(t(col2rgb(col)/2), maxColorValue = 255)
   # col_light <- rgb(t(255 - ((255 - col2rgb(col))/10)), maxColorValue = 255)
-  
+
   angl <- ifelse(n_cuts >= 8, 20, 0)
-  
+
   balance_capt <- ifelse(table_list$balance,
                          "et les primes sont balancees.",
                          "")
-  
+
   table_to_graph <- reshape2::melt(table,
                                    measure.vars = c("LR_ref",
-                                                    "LR_comp")) %>% 
+                                                    "LR_comp")) %>%
     mutate(r_group3 = factor(r_group, levels = unique(r_group)))
-  
+
   g <- ggplot(table_to_graph, aes(x = r_group3,
                                   y = value,
                                   colour = variable,
                                   group = variable)) +
     geom_path(alpha = 0.8, lwd = 1) +
-    geom_point(size = 2) + 
+    geom_point(size = 2) +
     theme_bw() +
-    scale_y_continuous(limits = c(min(table_to_graph$value) - 0.1, max(table_to_graph$value) + 0.1) ,labels = scales::percent) + 
-    
+    scale_y_continuous(limits = c(min(table_to_graph$value) - 0.1, max(table_to_graph$value) + 0.1) ,labels = scales::percent) +
+
     labs(title = "Ratio de perte en fonction du groupe de relativite",
          subtitle = paste0("Les relativites sont calculees en divisant les primes ",
                            Prem_names[2],
@@ -43,15 +58,15 @@ dbl_lift_chart <- function(table_list,
                           " sont les primes de reference ", balance_capt),
          x="Relativite",
          y="Ratio de perte") +
-    
-    theme( axis.text.x = element_text(angle = angl)) + 
+
+    theme( axis.text.x = element_text(angle = angl)) +
     scale_color_brewer(palette = palette,
                        name="Modele",
                        breaks=c("LR_ref", "LR_comp"),
                        labels=c(paste0(table_list$ref_name,
                                        " (ref)"),
                                 paste0(table_list$comp_name,
-                                       " (comp)"))) + 
+                                       " (comp)"))) +
     geom_hline(yintercept=1, alpha = 0.4, lty = 2, size =0.6 )
   ## "Viridis", "Plasma, "Purple-Orange"
   ## "Zissou1", "SunsetDark"
--- R/get_lr_table.R
+++ R/get_lr_table.R
@@ -11,71 +11,82 @@
 get_lr_table <- function(pred_table,
                          ref_name,
                          comp_name,
-                         expo_name = "Exposure",
-                         loss_name = "ClaimNbResp",
+                         expo_name = NULL,
+                         loss_name = NULL,
                          n_cuts = 10,
                          balance = TRUE){
-  
+
+  if(is_null(loss_name)) stop("the user must specify the response variable using the 'loss_name' argument")
+  if(is_null(expo_name)) warning("Since no exposure is specified, the exposure is assume to be constant accross observations")
+
   tbl <- data.frame("P_ref" = pred_table[[ref_name]],
                     "P_comp" = pred_table[[comp_name]],
-                    "expo" = pred_table[[expo_name]],
                     "loss" = pred_table[[loss_name]])
-  
+
+  ## if there is a specified exposure
+  if(!is_null(expo_name)){
+    ## add it to the table
+    tbl$expo <- pred_table[[expo_name]]
+  } else {
+    ## assume constant exposure
+    tbl$expo <- rep(1, nrow(pred_table))
+  }
+
   if(balance){
     tbl$P_ref <- tbl$P_ref * (mean(tbl$loss)/mean(tbl$P_ref))
     tbl$P_comp <- tbl$P_comp * (mean(tbl$loss)/mean(tbl$P_comp))
   }
-  
+
   ## 2. On calcule les relativites
   tbl$rr <- tbl$P_comp/tbl$P_ref
-  
+
   ## Si on a pas assez de diversite
   is_not_enough_values_of_rel <- (tbl$rr %>% round(5) %>% table %>% length)/n_cuts <= 1
   if(is_not_enough_values_of_rel){
     tbl$rr2 <- tbl$rr %>% round(4)
-    
+
     ## 3. Trier les individus
     sort_id <- sort.int(tbl$rr2, index.return = TRUE)$ix
-    
+
     ## trier la base de donnee
     test_sort <- tbl[sort_id, ]
-    
+
     ## values
     values <- test_sort$rr2 %>% table %>% names %>%  as.numeric()
-    
+
     test_sort$r_group2 <- sapply(1:nrow(test_sort), function(obs){
       which(test_sort$rr2[obs] == values)
     })
     test_sort$r_group <- values[test_sort$r_group2]
-    
+
     ## On renomme
     n_cuts <- length(values)
-    
+
   } else {
     ## 3. Trier les individus
     sort_id <- sort.int(tbl$rr, index.return = TRUE)$ix
     sorted_rr <- tbl$rr[sort_id]
-    
+
     ## 4. On cree 5 groupes qui contiennent la meme exposition
     expo_cumul <- cumsum(tbl$expo[sort_id])
     cut_points <- sum(tbl$expo)/n_cuts*(0:n_cuts) # La decision d'utiliser 5 groupes est arbitraire.
-    
+
     groups <- cut(expo_cumul, cut_points, labels = FALSE)
     # Ici, on veut simplement avoir des valeurs de 1 a 5 dependamment du groupe (quintile) d'appartenance
-    
+
     ## We assign the labels correctly
     new_groups <- put_labels(groups, sorted_rr)
-    
-    ## 5. On calcule le Loss Ratio par groupe 
+
+    ## 5. On calcule le Loss Ratio par groupe
     test_sort <- tbl[sort_id, ]
     test_sort$r_group <- new_groups
-    test_sort$r_group2 <- groups 
+    test_sort$r_group2 <- groups
   }
-  
+
   LossRatio_table <- aggregate(cbind(P_ref, P_comp, loss, expo) ~ r_group + r_group2, data = test_sort, FUN = sum)
   LossRatio_table$LR_ref <- LossRatio_table$loss/LossRatio_table$P_ref
   LossRatio_table$LR_comp <- LossRatio_table$loss/LossRatio_table$P_comp
-  
+
   l <- list("table" = LossRatio_table,
             "balance" = balance,
             "ref_name" = ref_name,
