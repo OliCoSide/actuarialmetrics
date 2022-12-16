f3b7c7b4c32956e7862f48248fbc3f98092e078b
Olivier Côté <45428376+OliCoSide@users.noreply.github.com>
2022-12-16 13:37:12 -0500

    trying to import ggplot2

---
+++ R/actuarialmetrics-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+
+#' @import reshape2 ggplot2  geneplotter
+#' @import survcomp gplots pheatmap RColorBrewer
+
+## usethis namespace: end
+NULL
